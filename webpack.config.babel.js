3d0a58df48d576a3b91d7666f462a46fc11e078b
粽子 <rail_0039@126.com>
2021-01-01 19:42:18 +0800

    del: 删除不需要的日志文件和代码片段

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import webpack from 'webpack'
 export default ({
-  entry: ['webpack-hot-middleware/client?noInfo=true&reload=true&timeout=100', './js/index08.js'],
+  entry: ['webpack-hot-middleware/client?noInfo=true&reload=true&timeout=100', './js/index.js'],
   output: {
     path: path.join(__dirname, '/static/'),
     filename: 'index.min.js'
